a0104dc684a660e67702deb1f7a6e66be55183c8
Viet-Trung Luu <vtl@tricot.io>
2019-06-05 11:21:50 -0700

    [syncherator] Add the ability to sync files from URLs, etc.
    The intended use is something like:
    
    FILENAME = 'toolchain.tar.gz'
    URL = 'https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz'
    SHA256= '3fe96c151d46c1d4e5edc6ed690851b8e53634041114bad04729bc16b0445156'
    got_new = sync_file_from_url('downloads', FILENAME, URL, SHA256)
    if got_new or not path_exists('stuff/xtensa-esp32-elf'):
        run_external('mkdir', '-p', 'stuff')
        run_external('rm', '-rf', 'stuff/xtensa-esp32-elf')
        run_external('tar', 'xfC', 'downloads/'+FILENAME, 'stuff')
    
    (I guess if you do this more than once, you might want to encapsulate it
    into a function.)

--- syncherator.py
+++ syncherator.py
@@ -5,17 +5,20 @@
 """Executes syncherator scripts, which are scripts to sync specified repos."""
 
 
+import hashlib
 import os
 import os.path
 import subprocess
 import sys
 
+import requests
+
 
 assert sys.version_info >= (3, 5), 'Python 3.5 or greater required'
 
 
-def _read_file(filename):
-    with open(filename) as file_to_read:
+def _read_file(filename, **kwargs):
+    with open(filename, **kwargs) as file_to_read:
         return file_to_read.read()
 
 
@@ -90,9 +93,39 @@ def syncherate(filename):
         for name, spec in _repos.items():
             _sync_one(destdir, name, spec)
 
+    def _sync_file_from_url(destdir, filename, url, sha256):
+        destdir = _maybe_abs(destdir)
+        os.makedirs(destdir, exist_ok=True)
+        destfile = os.path.join(destdir, filename)
+
+        print('[{}] sync file: {} <- {}; SHA256={}'.format(_cwd, destfile, url,
+                                                           sha256))
+        if os.path.exists(destfile):
+            existing_contents = _read_file(destfile, mode='rb')
+            have_sha256 = hashlib.sha256(existing_contents).hexdigest()
+            if have_sha256 == sha256:
+                return False
+
+        print('[{}] download: {} <- {}; SHA256={}'.format(_cwd, destfile, url,
+                                                          sha256))
+        resp = requests.get(url, allow_redirects=True)
+        if resp.status_code != 200:
+            raise RuntimeError(
+                'download {}: got status {}'.format(url, resp.status_code))
+        got_sha256 = hashlib.sha256(resp.content).hexdigest()
+        if got_sha256 != sha256:
+            raise RuntimeError('download {}: got SHA256 {}, expected {}'.format(
+                url, got_sha256, sha256))
+        with open(destfile, mode='wb') as file_to_write:
+            file_to_write.write(resp.content)
+        return True
+
     def _run_external(command, *args, cwd=None):
         cwd = _maybe_abs(cwd) if cwd else _cwd
-        _run(_maybe_abs(command), *args, cwd=cwd)
+        _run(command, *args, cwd=cwd)
+
+    def _path_exists(path):
+        return os.path.exists(_maybe_abs(path))
 
     global_vars = {
         '__builtins__': None,
@@ -105,7 +138,9 @@ def syncherate(filename):
         'remove_repo': _remove_repo,
         'clear_repos': _clear_repos,
         'sync': _sync,
+        'sync_file_from_url': _sync_file_from_url,
         'run_external': _run_external,
+        'path_exists': _path_exists,
     }
     # pylint: disable=exec-used
     exec(_read_file(filename), global_vars, local_vars)
